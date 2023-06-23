83f03e30f137a40a21c3f9f02103824db03bf293
valmojr <val_mo_jr@hotmail.com>
2023-06-23 11:10:35 -0300

    corrigido webhook handlers

--- webhooks/success.js
+++ webhooks/success.js
@@ -1,11 +1,11 @@
 // generate a script to send a HTTP post request to a discord webhook
 // example command to run this script: node success.js succes_pbo
 
-discord_webhook = "https://discord.com/api/webhooks/1073007808334803036/LGs14Mx4ko-H5qIZPK4q-4pdiCf1TMTpm9CfN4SOYSYwR_ClqNxPwsPk6FkKV4j4AQsh";
+const discord_webhook = "https://discord.com/api/webhooks/1073007808334803036/LGs14Mx4ko-H5qIZPK4q-4pdiCf1TMTpm9CfN4SOYSYwR_ClqNxPwsPk6FkKV4j4AQsh";
 
-sucess_pbo = process.argv[2];
+const sucess_pbo = process.argv[2];
 
-content = `PBO ${error_pbo} fechado com sucesso`;
+const content = `PBO ${error_pbo} fechado com sucesso`;
 
 fetch(discord_webhook, {
     method: 'POST',
