467d53c0c83da7a139a72f811ebfd1b98ff3e166
valmojr <val_mo_jr@hotmail.com>
2023-06-23 09:56:58 -0300

    teste 11: HOOK FUNCIONOU, pboproject não achou pasta 8

--- braf_testpbo/config.cpp
+++ braf_testpbo/config.cpp
@@ -10,7 +10,7 @@ class CfgPatches
         };
         requiredVersion = 0.1;
         requiredAddons[] = {
-            "test1212iu33putaiu",
+            "test1212iu33aiu",
         };
     };
 };
