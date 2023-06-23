928032db4be40138ba5933a1fb6a7f86bf112781
valmojr <val_mo_jr@hotmail.com>
2023-06-23 14:15:49 -0300

    atualizado webhooks

--- webhooks/error.js
+++ webhooks/error.js
@@ -1,12 +1,8 @@
 async function sendDiscordMessage() {
     const discord_webhook = "https://discord.com/api/webhooks/1073007808334803036/LGs14Mx4ko-H5qIZPK4q-4pdiCf1TMTpm9CfN4SOYSYwR_ClqNxPwsPk6FkKV4j4AQsh";
 
-    console.log("process: " + process.argv[2]);
-
     const error_pbo = process.argv[2];
     const error_message = process.argv[3];
-
-    console.log("process: " + error_pbo);
 
     const content = `Erro ao fechar o PBO ${error_pbo}` + "```" + error_message + "```";
 
--- webhooks/success.js
+++ webhooks/success.js
@@ -1,18 +1,20 @@
 // generate a script to send a HTTP post request to a discord webhook
 // example command to run this script: node success.js succes_pbo
 
-const discord_webhook = "https://discord.com/api/webhooks/1073007808334803036/LGs14Mx4ko-H5qIZPK4q-4pdiCf1TMTpm9CfN4SOYSYwR_ClqNxPwsPk6FkKV4j4AQsh";
+async function sendDiscordMessage() {
+    const discord_webhook = "https://discord.com/api/webhooks/1073007808334803036/LGs14Mx4ko-H5qIZPK4q-4pdiCf1TMTpm9CfN4SOYSYwR_ClqNxPwsPk6FkKV4j4AQsh";
 
-const sucess_pbo = process.argv[2];
+    const error_pbo = process.argv[2];
 
-const content = `PBO ${error_pbo} fechado com sucesso`;
+    const content = `PBO ${error_pbo} fechado com sucesso`;
 
-fetch(discord_webhook, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        content
-    })
-});
+    await fetch(discord_webhook, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            content
+        })
+    });
+}
