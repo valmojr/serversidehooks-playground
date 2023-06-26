1fdbb43e8b2dbab7c9b2dbe4ca32c282ee8f4e5a
valmojr <val_mo_jr@hotmail.com>
2023-06-26 00:04:23 -0300

    ok

--- hooks/error.js
+++ hooks/error.js
@@ -2,7 +2,7 @@ async function sendDiscordMessage() {
     const discord_webhook = "https://discord.com/api/webhooks/1073007808334803036/LGs14Mx4ko-H5qIZPK4q-4pdiCf1TMTpm9CfN4SOYSYwR_ClqNxPwsPk6FkKV4j4AQsh";
 
     const error_pbo = process.argv[2];
-    const error_message = process.argv[3];
+    const error_message = "O PBO " + error_pbo + "não foi enpacotado corretamente, verifique se está usando a versão correta do mikero";
 
     const content = `⛔ ${error_pbo}\n\n` + "```\n" + error_message + "\n```";
 
