d868c3a7779442e240e362da8a1359a4ebf50c27
valmojr <val_mo_jr@hotmail.com>
2023-06-26 00:10:52 -0300

    ok

---
+++ braf_testpbo4/config.cpp
@@ -0,0 +1,24 @@
+class CfgPatches
+{
+    class testpbo4
+    {
+        units[] = {
+
+        };
+        weapons[] = {
+
+        };
+        requiredVersion = 0.1;
+        requiredAddons[] = {
+            "qwdqwdqitq",
+        };
+    };
+};
+
+class CfgVehicles
+{
+    class testeveiculo
+    {
+        model = "\serversidehookstesting\braf_testpbo4\config.p3d"
+    };
+};
